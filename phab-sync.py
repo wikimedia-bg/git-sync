175f70e1a6850e438db1fe7043c6beb46ba080bf
Luchesar V. ILIEV <luchesar.iliev@gmail.com>
2018-10-21 21:42:37 +0300

    [phab2wiki sync] implement bidirectional sync
    We'll be using PSS 9 for the tests, as MW requires admin access.
    This also means that JSS 9 must be _both_ interface-editor and sysop.

--- phab-sync.py
+++ phab-sync.py
@@ -13,8 +13,8 @@ import git
 import pywikibot as pwb
 
 
-BOT_USERNAME = 'JSS 9'
-SLEEP_SEC = 60
+BOT_USERNAME = 'PSS 9'
+SLEEP_SEC = 10
 
 
 class SignalHandler:
@@ -47,11 +47,11 @@ class SignalHandler:
 class PhabRepo:
 
     def __init__(self, repo, site, ns, t_re):
-        self._changed_paths = []
         self.repo = repo
         self.site = site
         self.ns = ns
         self.t_re = t_re
+        self._pending_files = []
 
     def _allpages(self):
         return self.site.allpages(namespace=self.ns)
@@ -69,19 +69,20 @@ class PhabRepo:
         revs.sort(key=lambda rev: rev[1]['timestamp'])
         return revs
 
-    def pull(self):
+    def _pull(self):
         old_master = self.repo.commit('master')
         self.repo.git.pull()
         # Obtain all changed files in this pull.
-        self._changed_paths += self.repo.git.diff_tree(
+        self._pending_files += self.repo.git.diff_tree(
                 '--no-commit-id',
                 '--name-only',
                 '-r',
                 old_master,
                 self.repo.commit('master')).split('\n')
 
-    def update(self):
-        for rev in self._revlist():
+    def _wiki2phab(self):
+        revs = self._revlist()
+        for rev in revs:
             if rev[1]['user'] == BOT_USERNAME:
                 continue
             elif rev[1]['user'] == 'Iliev':
@@ -97,7 +98,7 @@ class PhabRepo:
             os.makedirs(os.path.dirname(file_path), exist_ok=True)
             # To avoid conflicts as much as possible, perform git pull right before we apply the
             # change and commit it.
-            self.pull()
+            self._pull()
             with open(file_path, 'w') as f:
                 f.write(rev[1]['text'])
             self.repo.index.add([file_path])
@@ -111,11 +112,36 @@ class PhabRepo:
                     commit_date=str(rev[1]['timestamp'])[:-1])
             # Push after each commit. It's inefficient, but should minimize possible conflicts.
             self.repo.git.push()
+        # Return the list of files that have been synced from Wikipedia.
+        return [_[0].replace('/', '.d/') for _ in revs]
+
+    def _phab2wiki(self, files_tosync):
+        for file_name in files_tosync:
+            file_path = os.path.join(self.repo.working_dir, file_name)
+            page = pwb.Page(self.site, self.ns + ':' + file_name.replace('.d/', '/'))
+            with open(file_path, 'r') as f:
+                page.text = f.read()
+            page.save('Sync from Phabricator', botflag=True)
+
+    def sync(self):
+        w2ph_synced_files = self._wiki2phab()
+        self._pull()
+        # If no files have been changed in Phabricator, self._pending_files is set to [''] (list
+        # with a single element, which itself is an empty string). This is _not_ an empty list.
+        ph2w_pending_files = list(filter(None, self._pending_files))
+        # Determine which pages need to be updated on Wikipedia. To be on the safe side, if during
+        # one sync operation a page is changed both on Wikipedia and on Phabricator, the Wikipedia
+        # version wins. We do this by simply substracting the list of files that were updated on
+        # Wikipedia from the list of files updated on Phabricator (converting to a set() first).
+        ph2w_files_tosync = set(ph2w_pending_files) - set(w2ph_synced_files)
+        if ph2w_files_tosync:
+            self._phab2wiki(ph2w_files_tosync)
+            self._pending_files = []
 
 
-def sync_thread(repos):
+def sync_repos(repos):
     for repo in repos:
-        repo.update()
+        repo.sync()
 
 
 def main(argv):
@@ -158,7 +184,7 @@ def main(argv):
 
     while True:
         print('Running...')
-        sync_thread(repos)
+        sync_repos(repos)
         print('Sleeping...')
         sig_handler.sleep()
 
